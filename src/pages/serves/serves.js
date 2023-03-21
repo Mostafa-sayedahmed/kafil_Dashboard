f6c8d6dfb092931ad431a6442e17615602a66103
Bassam Mohamed Ali <120774533+bassamradwan@users.noreply.github.com>
2023-03-22 00:48:24 +0200

    last  change

--- src/pages/serves/serves.js
+++ src/pages/serves/serves.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cardpoject from "../../components/CardProject/Cardpoject";
 import Smpilcard from "../../components/smpilcard/smpilcard";
 import "../serves/serves.css";
 function Serves() {
@@ -7,7 +8,7 @@ function Serves() {
       <div className="bg-white border border-secondary-subtle  rounded ">
         <h1 className="m-2">الخدمات</h1>
       </div>
-      <div className="B-serves p-5 ">
+      <div className="B-serves p-3 ">
         <Smpilcard cardName="الاجمالى الخدمات" cardValue="0" />
         <Smpilcard cardName="بانتظار موافقة الادارة" cardValue="0" />
         <Smpilcard cardName="يحتاج الى تعديلات" cardValue="0" />
@@ -18,6 +19,33 @@ function Serves() {
       <div className="bg-white border border-secondary-subtle mt-3 rounded">
         <h1 className="m-2">الخدمات</h1>
       </div>
+      <table className="table table-hover bg-white">
+  
+  <thead>
+  <tr>
+    <th >#</th>
+    <th >NameProject</th>
+    <th >NamePerson</th>
+    <th >Time</th>
+    <th >budget</th>
+    <th >Delete</th>
+  </tr>
+</thead>
+  <tbody>
+  <Cardpoject hash="1" NameProject="مصمم ويب" NamePerson="Sami Samir" Time="3" budget="$25 - $50"/>
+  <Cardpoject hash="1" NameProject="مصمم ويب" NamePerson="Sami Samir" Time="3" budget="$25 - $50"/>   
+   <Cardpoject hash="2" NameProject="نحتاج الي مطور ومكود بلوجر " NamePerson="Imen Amri" Time="5" budget="$25 - $50"/>   
+   <Cardpoject hash="3" NameProject=" مطلوب موقع الكتروني يكون ترتية جيد" NamePerson="Ahmed Hesham" Time="4" budget="$250 - $500"/>   
+  <Cardpoject hash="1" NameProject="مصمم ويب" NamePerson="Sami Samir" Time="3" budget="$25 - $50"/>   
+  <Cardpoject hash="1" NameProject="مصمم ويب" NamePerson="Sami Samir" Time="3" budget="$25 - $50"/>   
+  <Cardpoject hash="1" NameProject="مصمم ويب" NamePerson="Sami Samir" Time="3" budget="$25 - $50"/>   
+   <Cardpoject hash="2" NameProject="نحتاج الي مطور ومكود بلوجر " NamePerson="Imen Amri" Time="5" budget="$25 - $50"/>   
+   <Cardpoject hash="3" NameProject=" مطلوب موقع الكتروني يكون ترتية جيد" NamePerson="Ahmed Hesham" Time="4" budget="$250 - $500"/>
+   <Cardpoject hash="1" NameProject="مصمم ويب" NamePerson="Sami Samir" Time="3" budget="$25 - $50"/>   
+   <Cardpoject hash="2" NameProject="نحتاج الي مطور ومكود بلوجر " NamePerson="Imen Amri" Time="5" budget="$25 - $50"/>   
+   <Cardpoject hash="3" NameProject=" مطلوب موقع الكتروني يكون ترتية جيد" NamePerson="Ahmed Hesham" Time="4" budget="$250 - $500"/>   
+   </tbody>
+   </table>
     </div>
   );
 }
