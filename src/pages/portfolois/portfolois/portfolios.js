f58554aa291179c8fd8f6e451697b5ed55324288
Bassam Mohamed Ali <120774533+bassamradwan@users.noreply.github.com>
2023-04-09 03:49:01 +0200

    bassam

--- src/pages/portfolois/portfolois/portfolios.js
+++ src/pages/portfolois/portfolois/portfolios.js
@@ -1,128 +1,75 @@
-import React from 'react'
-import Cardpoject from '../../../components/CardProject/Cardpoject'
-import Smpilcard from '../../../components/smpilcard/smpilcard'
+import React, { useEffect, useState } from 'react';
+import Cardpoject from '../../components/CardProject/Cardpoject';
+import Smpilcard from '../../components/smpilcard/smpilcard';
+import { db } from '../../Firebase/Firebase';
+function Portfolios() {
+  const [Portfolios, setportfolios] = useState([]);
 
- function Portfolios() {
+  useEffect(() => {
+    const getDataportfolio = [];
+    const subscriber = db.collection('protfolios').onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        getDataportfolio.push({ ...doc.data() });
+      });
+      setportfolios(getDataportfolio);
+    });
+
+    return () => subscriber();
+  }, []);
+
+  const getTotalPortfolios = (portfolios) => {
+    return portfolios.reduce((accumulator, current) => {
+      return accumulator + 1;
+    }, 0);
+  };
 
 
   
+  console.log(Portfolios);
+
   return (
-    <div className=" ">
-      <div className="bg-white border border-secondary-subtle  rounded ">
+    <div className=" " style={{color:"#32383e"}}>
+      <div className="bg-white border border-secondary-subtle rounded ">
         <h1 className="m-2">الأعمال</h1>
       </div>
+
       <div className="B-serves p-3 ">
-        <Smpilcard cardName="الاجمالى الأعمال" cardValue="0" />
-        <Smpilcard cardName="بانتظار موافقة " cardValue="0" />
-        <Smpilcard cardName="يحتاج الى تعديلات" cardValue="0" />
-        <Smpilcard cardName="نشط" cardValue="0" />
-        <Smpilcard cardName=" متوقف مؤقتا" cardValue="0" />
-        <Smpilcard cardName="مرفوض" cardValue="0" />
+      <Smpilcard cardName="الاجمالى الأعمال" cardValue={getTotalPortfolios(Portfolios)} />
+         <Smpilcard cardName="بانتظار موافقة الأدارة" cardValue="2" />
+        <Smpilcard cardName="يحتاج الى تعديلات" cardValue="4" />
+         <Smpilcard cardName="نشط" cardValue="10" />
+         <Smpilcard cardName=" متوقف مؤقتا" cardValue="16" />
+         <Smpilcard cardName="مرفوض" cardValue="20" />
       </div>
-      <div className="services-container bg-white border border-secondary-subtle mt-3 rounded">
+      <div className="services-container bg-white border border-secondary-subtle mt-3 rounded"
+      >
         <h1 className="m-2">الأعمال</h1>
-        {/* <Container > */}
-        <table className="table table-hover bg-white services-table">
-          <thead>
+        <hr/>
+        <table className="table table-hover bg-white services-table ">
+          <thead >
             <tr>
-              <th>#</th>
-              <th>اسم الخدمة</th>
-              <th>صاحب الخدمة</th>
-              <th>الوقت</th>
-              <th>الميزانية</th>
+              <th>صاحب العمل</th>
+              <th>اسم العمل </th>
               <th>تحكم في الخدمة</th>
             </tr>
           </thead>
           <tbody>
-            <Cardpoject
-              hash="1"
-              NameProject="مصمم ويب"
-              NamePerson="Sami Samir"
-              Time="3"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="1"
-              NameProject="مصمم ويب"
-              NamePerson="Sami Samir"
-              Time="3"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="2"
-              NameProject="نحتاج الي مطور ومكود بلوجر "
-              NamePerson="Imen Amri"
-              Time="5"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="3"
-              NameProject=" مطلوب موقع الكتروني يكون ترتية جيد"
-              NamePerson="Ahmed Hesham"
-              Time="4"
-              budget="$250 - $500"
-            />
-            <Cardpoject
-              hash="1"
-              NameProject="مصمم ويب"
-              NamePerson="Sami Samir"
-              Time="3"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="1"
-              NameProject="مصمم ويب"
-              NamePerson="Sami Samir"
-              Time="3"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="1"
-              NameProject="مصمم ويب"
-              NamePerson="Sami Samir"
-              Time="3"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="2"
-              NameProject="نحتاج الي مطور ومكود بلوجر "
-              NamePerson="Imen Amri"
-              Time="5"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="3"
-              NameProject=" مطلوب موقع الكتروني يكون ترتية جيد"
-              NamePerson="Ahmed Hesham"
-              Time="4"
-              budget="$250 - $500"
-            />
-            <Cardpoject
-              hash="1"
-              NameProject="مصمم ويب"
-              NamePerson="Sami Samir"
-              Time="3"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="2"
-              NameProject="نحتاج الي مطور ومكود بلوجر "
-              NamePerson="Imen Amri"
-              Time="5"
-              budget="$25 - $50"
-            />
-            <Cardpoject
-              hash="3"
-              NameProject=" مطلوب موقع الكتروني يكون ترتية جيد"
-              NamePerson="Ahmed Hesham"
-              Time="4"
-              budget="$250 - $500"
-            />
+            {Portfolios.map((portfolio, index) => (
+              <tr key={index}>
+                <td><i class="fa-solid fa-user ms-2" style={{color: "#9ca1ab"}} ></i>{portfolio.name}</td>
+                <td> <i class="fa-solid fa-list-check ms-2" style={{color: "#9ca1ab"}}></i>{portfolio.protfoloiName}</td>
+                <td>
+                <button className='btn btn-outline-danger p-2 rounded'>
+              Delete
+            </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
-        {/* </Container> */}
       </div>
     </div>
-  )
+  );
 }
-export default Portfolios
+
+export default Portfolios;
