814700de016169faeb82b6786e1a93e562d353ef
Mostafa-sayedahmed <107958289+Mostafa-sayedahmed@users.noreply.github.com>
2023-04-10 13:36:07 +0200

    Update protectedRoute.js

--- src/components/protectedRoute/protectedRoute.js
+++ src/components/protectedRoute/protectedRoute.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({isLoggedIn,children}) => {
-
-     if(isLoggedIn == 'true'){
-        return children
-     }else{
-       return <Navigate to='/'/>
-     }
-}
+const ProtectedRoute = ({ isLoggedIn, children }) => {
+  if (isLoggedIn == "true") {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
 
 export default ProtectedRoute;
